b3bb77a36528e5fe018d8290868ef820eac52083
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-20 19:30:46 +0000

    strings(regular expressions)

---
+++ src/__test__/Using Matches/strings/strings.test.js
@@ -0,0 +1,11 @@
+// You can check strings against regular expressions with toMatch:
+
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect("Christoph").toMatch(/stop/);
+});
+
+// npm test strings.test.js
