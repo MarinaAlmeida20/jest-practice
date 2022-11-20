6355be7567af90b98337fe799ca1eb3ce7d15c41
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-20 19:19:28 +0000

    truthiness

---
+++ src/__test__/Using Matches/truthiness/truthiness.test.js
@@ -0,0 +1,32 @@
+/*
+In tests, you sometimes need to distinguish between 
+undefined, null, and false, but you sometimes do not want 
+to treat these differently. Jest contains helpers that let 
+you be explicit about what you want.
+*/
+
+/*
+ * toBeNull matches only null
+ * toBeUndefined matches only undefined
+ * toBeDefined is the opposite of toBeUndefined
+ * toBeTruthy matches anything that an if statement treats as true
+ * toBeFalsy matches anything that an if statement treats as false
+ */
+
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
